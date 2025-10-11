487cc308bb095e7c7f80432a0cb698baf1b765e7
sonamargaryan <sonamargaryan764@gmail.com>
2025-10-11 12:44:50 +0400

    Create homework1(xor).cpp

---
+++ homework1(xor).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main () {
+    int arr [] = {0, 2, 3, 8, 7, 7, 1, 1, 0, 8, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    int result = 0;
+    for (int i = 0; i < n; i ++)
+        result ^= arr[i];
+        
+    std::cout << "Single number is: " << result << std::endl;
+    return 0;
+}
