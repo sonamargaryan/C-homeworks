d4d537df4c5a5b7097da8c6474da0df7ac9200c2
sonamargaryan <sonamargaryan764@gmail.com>
2025-12-15 00:15:18 +0300

    Create friend_func.cpp

---
+++ Mid Exam 2/Practising/Friend/Friend function/friend_func.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class EquilateralTriangle {
+    private:
+        float a;
+        float circumference;
+        float area;
+    public:
+        void setA (float length) {
+            a = length;
+            circumference = 3 * a;
+            area = (1.73 * a * a) / 4;
+        }
+
+        friend void PrintResults (EquilateralTriangle);
+};
+
+void PrintResults (EquilateralTriangle obj) {
+    cout << "Circumference = " << obj.circumference << "\n";
+    cout << "Area = " << obj.area << "\n";
+}
+
+int main () {
+	EquilateralTriangle object;
+    object.setA (3);
+
+    PrintResults (object);
+
+	return 0;
+}
